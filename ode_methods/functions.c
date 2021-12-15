9a1626d57fd307c389f1d22ad0ba6c9adaa6114d
Santurysim <santurysim@gmail.com>
2021-12-15 22:40:58 +0300

    Fix yet more warnings

--- ode_methods/functions.c
+++ ode_methods/functions.c
@@ -2,18 +2,29 @@
 
 #include <assert.h>
 
-double f1(double /* x */, double *y, int n) {
+double f1(double, double*, int);
+double f2(double, double*, int);
+double f3(double, double*, int);
+
+double ref1(double);
+double ref2(double);
+double ref3(double);
+
+double f1(double x, double *y, int n) {
 	assert(n >= 3);
+	(void)x;
 	return 3 * y[0] - y[1] + y[2];
 }
 
-double f2(double /* x */, double *y, int n) {
+double f2(double x, double *y, int n) {
 	assert(n >= 3);
+	(void)x;
 	return y[0] + y[1] + y[2];
 }
 
-double f3(double /* x */, double *y, int n) {
+double f3(double x, double *y, int n) {
 	assert(n >= 3);
+	(void)x;
 	return 4 * y[0] - y[1] + 4 * y[2];
 }
 
